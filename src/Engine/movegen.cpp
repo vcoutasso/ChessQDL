f5e6714b1a7c97a9400c9b2b3bb65aabe51d2ccc
Vinícius Couto <vinicouto12@gmail.com>
2019-11-11 00:02:40 -0300

    Implementation of bitboard shift operations

--- src/Engine/movegen.cpp
+++ src/Engine/movegen.cpp
@@ -2,3 +2,82 @@
 
 using namespace chessqdl;
 
+/**
+ * @details shifts bitboard north. E.g
+ * 0 0 0	0 1 0
+ * 0 1 0 -> 0 0 0
+ * 0 0 0	0 0 0
+ */
+U64 MoveGenerator::shiftNorth(U64 bitboard) {
+	return bitboard << 8;
+}
+
+/**
+ * @details shifts bitboard northeast. E.g
+ * 0 0 0	0 0 1
+ * 0 1 0 -> 0 0 0
+ * 0 0 0	0 0 0
+ */
+U64 MoveGenerator::shiftNorthEast(U64 bitboard) {
+	return bitboard << 7;
+}
+
+/**
+ * @details shifts bitboard east. E.g
+ * 0 0 0	0 0 0
+ * 0 1 0 -> 0 0 1
+ * 0 0 0	0 0 0
+ */
+U64 MoveGenerator::shiftEast(U64 bitboard) {
+	return bitboard >> 1;
+}
+
+/**
+ * @details shifts bitboard southeast. E.g
+ * 0 0 0	0 0 0
+ * 0 1 0 -> 0 0 0
+ * 0 0 0	0 0 1
+ */
+U64 MoveGenerator::shiftSouthEast(U64 bitboard) {
+	return bitboard >> 7;
+}
+
+/**
+ * @details shifts bitboard south. E.g
+ * 0 0 0	0 0 0
+ * 0 1 0 -> 0 0 0
+ * 0 0 0	0 1 0
+ */
+U64 MoveGenerator::shiftSouth(U64 bitboard) {
+	return bitboard >> 8;
+}
+
+/**
+ * @details shifts bitboard south. E.g
+ * 0 0 0	0 0 0
+ * 0 1 0 -> 0 0 0
+ * 0 0 0	1 0 0
+ */
+U64 MoveGenerator::shiftSouthWest(U64 bitboard) {
+	return bitboard >> 9;
+}
+
+/**
+ * @details shifts bitboard west. E.g
+ * 0 0 0	0 0 0
+ * 0 1 0 -> 1 0 0
+ * 0 0 0	0 0 0
+ */
+U64 MoveGenerator::shiftWest(U64 bitboard) {
+	return bitboard << 1;
+}
+
+/**
+ * @details shifts bitboard northwest. E.g
+ * 0 0 0	1 0 0
+ * 0 1 0 -> 0 0 0
+ * 0 0 0	0 0 0
+ */
+U64 MoveGenerator::shiftNorthWest(U64 bitboard) {
+	return bitboard << 9;
+}
--- src/Engine/movegen.h
+++ src/Engine/movegen.h
@@ -10,22 +10,61 @@ namespace chessqdl {
 	public:
 		MoveGenerator() = default;
 
+		/**
+		 * @brief Shifts bitboard one up and returns it
+		 * @param bitboard - bitboard to be shifted
+		 * @return Bitboard shifted one up
+		 */
 		U64 shiftNorth(U64 bitboard);
 
+		/**
+		 * @brief Shifts bitboard one up, one right and returns it
+		 * @param bitboard - bitboard to be shifted
+		 * @return Bitboard shifted one up one right
+		 */
 		U64 shiftNorthEast(U64 bitboard);
 
+		/**
+		 * @brief Shifts bitboard one right and returns it
+		 * @param bitboard - bitboard to be shifted
+		 * @return Bitboard shifted one right
+		 */
 		U64 shiftEast(U64 bitboard);
 
+		/**
+		 * @brief Shifts bitboard one right one down and returns it
+		 * @param bitboard - bitboard to be shifted
+		 * @return Bitboard shifted one right one down
+		 */
 		U64 shiftSouthEast(U64 bitboard);
 
+		/**
+		 * @brief Shifts bitboard one down and returns it
+		 * @param bitboard - bitboard to be shifted
+		 * @return Bitboard shifted one down
+		 */
 		U64 shiftSouth(U64 bitboard);
 
+		/**
+		 * @brief Shifts bitboard one down one left and returns it
+		 * @param bitboard - bitboard to be shifted
+		 * @return Bitboard shifted one down one left
+		 */
 		U64 shiftSouthWest(U64 bitboard);
 
+		/**
+		 * @brief Shifts bitboard one left and returns it
+		 * @param bitboard - bitboard to be shifted
+		 * @return Bitboard shifted one left
+		 */
 		U64 shiftWest(U64 bitboard);
 
+		/**
+		 * @brief Shifts bitboard one left one up and returns it
+		 * @param bitboard - bitboard to be shifted
+		 * @return Bitboard shifted one left one up
+		 */
 		U64 shiftNorthWest(U64 bitboard);
-
 
 
 	};
